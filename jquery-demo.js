1e002e6b71c3da5fe8e7ad97aeabb4080a77e9f1
jocunda <silviawanshan88@gmail.com>
2021-08-09 22:04:38 +0800

    jquery

---
+++ jquery-demo.js
@@ -0,0 +1,29 @@
+class jQuery {
+    constructor(selector) {
+        const result = document.querySelectorAll(selector)
+        const length = result.length
+        for (let i = 0; i < length; i++) {
+            this[i] = result[i]
+        }
+        this.length = length
+        this.selector = selector
+    }
+    get(index) {
+        return this[index]
+    }
+    each(fn) {
+        for (let i = 0; i < this.length; i++) {
+            const elem = this[i]
+            fn(elem)
+        }
+    }
+    on(type, fn) {
+        return this.each(elem => {
+            elem.addEventListener(type, fn, false)
+        })
+    }
+}
+
+// const $p = new jQuery('p')
+// $p.get(1)
+// $p.on('click', () => alert('clicked'))
